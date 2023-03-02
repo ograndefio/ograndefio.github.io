fadea3ebb2552c9f637afa8adfa2a4219916c0b0
lucasmolinari <lucas.molinari2003@gmail.com>
2023-03-02 09:51:18 -0300

    music test

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
+var tag = document.createElement('script');
 
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYoutubeIframeAPIReady(){
+player = new YT.player('player',{
+    height: '360',
+    width: '640',
+    videoId: 'oowBXzfcl90',
+    events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+  }
 
