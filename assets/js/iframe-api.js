e55c3c5259e25bb5b08e18ad9a553b4d255c2278
lucasmolinari <lucas.molinari2003@gmail.com>
2023-03-18 02:51:09 -0300

    fix scirocco

--- assets/js/iframe-api.js
+++ assets/js/iframe-api.js
@@ -1,7 +1,7 @@
 let player;
 const lightLockId = "gRwgNC6R9-c"
 const videoNeocities = "he8fOtVDj4Y&t=2s"
-const sciroccoId = "he8fOtVDj4Y&t=2s"
+const sciroccoId = "he8fOtVDj4Y"
 
 let fileName = location.href.split("/").slice(-1);
 let videoId;
@@ -26,8 +26,8 @@ function onYouTubeIframeAPIReady() {
     console.log('api loaded')
 
     player = new YT.Player("player", {
-        height: 100,
-        width: 100,
+        height: 0.001,
+        width: 0.001,
         videoId: videoId,
         playerVars: { 'mute': 0, 'autoplay': 1 },
         events: {}
