c0b5d8c37745f34a9c1359ae714d864623da0004
lucasmolinari <lucas.molinari2003@gmail.com>
2023-03-18 02:35:48 -0300

    auto iframe test

--- assets/js/iframe-api.js
+++ assets/js/iframe-api.js
@@ -1,13 +1,32 @@
 let player;
-let lightLockId = "gRwgNC6R9-c"
-let videoNeocities = 'cReuQk0pJbI'
+const lightLockId = "gRwgNC6R9-c"
+const videoNeocities = "he8fOtVDj4Y&t=2s"
+const sciroccoId = "he8fOtVDj4Y&t=2s"
+
+let fileName = location.href.split("/").slice(-1);
+let videoId;
+
+console.log(fileName[0])
+switch(fileName[0]){
+    case 'heart.html':
+        videoId = lightLockId;
+        break
+    case 'ofio.html':
+        videoId = sciroccoId;
+        break
+
+    default:
+        videoId = videoNeocities;
+        break
+}
+
 function onYouTubeIframeAPIReady() {
     console.log('api loaded')
 
     player = new YT.Player("player", {
         height: 0.0001,
         width: 0.0001,
-        videoId: lightLockId,
+        videoId: videoId,
         playerVars: { 'mute': 0, 'autoplay': 1 },
         events: {}
     })
